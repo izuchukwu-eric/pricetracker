bfb0ec86cf8de2f15aa8b845f38afc4097d69cee
izuchukwu-eric <onukwubeizu@gmail.com>
2024-03-07 04:40:14 +0100

    added: bright data config, axios, cheerio

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,17 +1,56 @@
 "use client"
-import React from 'react'
+import React, { FormEvent, useState } from 'react'
 
 const SearchBar = () => {
-    const handleSubmit = () => {}
+    const [searchPrompt, setSearchPrompt] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const isValidAmazonProductUrl = (url: string) => {
+      try {
+        const parsedURL = new URL(url);
+        const hostname = parsedURL.hostname;
+
+        if(
+          hostname.includes('amazon.com') || 
+          hostname.includes('amazon.') || 
+          hostname.endsWith('amazon')
+        ) {
+          return true;
+        }
+      } catch (error) {
+        return false;
+      }
+      return false;
+    }
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+
+      const isValidLink = isValidAmazonProductUrl(searchPrompt);
+
+      if(!isValidLink) return alert('Please provide a valid Amazon link');
+
+      try {
+        setLoading(true);
+
+        // Scrape the product page
+        
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
   return (
     <form className='flex flex-wrap gap-4 mt-12' onSubmit={handleSubmit}>
         <input
             type='text'
             placeholder='Enter product link'
             className='searchbar-input'
+            onChange={(e) => setSearchPrompt(e.target.value)}
         />
-        <button type='submit' className='searchbar-btn'>
-          Search
+        <button type='submit' className='searchbar-btn' disabled={searchPrompt === ''}>
+          {loading ? 'Searching...' : 'Search'}
         </button>
     </form>
   )
